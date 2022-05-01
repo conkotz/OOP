73d6afa9b4d4428e2a094d13693aa463f6a7d205
conkotz <104710339+conkotz@users.noreply.github.com>
2022-05-01 16:44:10 +1000

    Add files via upload

---
+++ 1.2/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+
+            Message myMessage;
+            myMessage = new Message("Hello World - From Message object");
+            myMessage.Print();
+
+            Message[] messages;
+            messages = new Message[4];
+            messages[0] = new Message("Welcome Back"); 
+            messages[1] = new Message("What a lovely name");
+            messages[2] = new Message("Great name");
+            messages[3] = new Message("That is a silly name"); 
+
+            Console.WriteLine("Enter name: ");
+            string usersName = Console.ReadLine().ToLower();
+
+            if (usersName == "conrad")
+            {
+                messages[0].Print();           
+            }
+            else if (usersName == "james")
+            {
+                messages[1].Print();
+            }
+            else if (usersName == "john")
+            {
+                messages[2].Print();
+            }
+            else
+            {
+                messages[3].Print();
+            }
+        }
+    }
+}
